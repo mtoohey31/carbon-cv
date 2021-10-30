364d4f53ba31d6e9dd4092bbc18b39ca5394dfc8
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-29 21:13:51 -0400

    fix: create types/generated if it doesn't exist

--- scripts/compile-schema.js
+++ scripts/compile-schema.js
@@ -4,6 +4,17 @@ import { compile } from "json-schema-to-typescript";
 
 const schema = YAML.load(String(fs.readFileSync("schema.yaml")));
 
+try {
+  fs.accessSync("types/generated");
+} catch {
+  try {
+    fs.accessSync("types");
+  } catch {
+    fs.mkdirSync("types");
+  }
+  fs.mkdirSync("types/generated");
+}
+
 fs.writeFileSync(
   "types/generated/schema-type.ts",
   await compile(schema, "Recipe")
