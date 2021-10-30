b749a97ce585b5674fa7cecb4ed8346fbb648514
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-29 20:58:27 -0400

    fix: switch script imports to puppeteer-core

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -1,39 +1,40 @@
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import child_process from 'child_process';
+import puppeteer from "puppeteer-core";
+import fs from "fs";
+import child_process from "child_process";
 
 async function save(subprocess) {
   const browser = await puppeteer.launch({ executablePath: "/usr/bin/brave" });
   const page = await browser.newPage();
   // TODO: pull URL from stdout of dev command
-  await page.goto('http://localhost:3000');
+  await page.goto("http://localhost:3000");
 
   try {
-    fs.accessSync('build');
+    fs.accessSync("build");
   } catch {
-    fs.mkdirSync('build');
+    fs.mkdirSync("build");
   }
 
   try {
-    fs.unlinkSync('build/cv.html');
-  } catch { } finally {
+    fs.unlinkSync("build/cv.html");
+  } catch {
+  } finally {
     const HTML = await page.content();
     // TODO: do this with a more intelligent tool
     const scriptLessHTML = HTML.replace(/<script.*<\/script>/gi, "");
-    fs.writeFileSync('build/cv.html', scriptLessHTML);
+    fs.writeFileSync("build/cv.html", scriptLessHTML);
     await browser.close();
-    console.log('html saved');
-    subprocess.kill('SIGINT');
+    console.log("html saved");
+    subprocess.kill("SIGINT");
     console.log("dev server stopped");
   }
 }
 
-const subprocess = child_process.spawn("npm", ["run", "dev"])
+const subprocess = child_process.spawn("npm", ["run", "dev"]);
 let saveBegun = false;
-subprocess.stdout.on('data', (data) => {
+subprocess.stdout.on("data", (data) => {
   if (!saveBegun && String(data).match(/> Local: http:\/\//g)) {
     saveBegun = true;
-    console.log("dev server started")
+    console.log("dev server started");
     save(subprocess);
   }
-})
+});
--- scripts/build-pdf.js
+++ scripts/build-pdf.js
@@ -1,56 +1,70 @@
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import child_process from 'child_process';
-import * as pdf2json from 'pdf2json';
+// TODO: improve logic and speed here, definitely with some sort of binary searching, and maybe with caching the last successful size
+import puppeteer from "puppeteer-core";
+import fs from "fs";
+import child_process from "child_process";
+import * as pdf2json from "pdf2json";
 
 async function pageCount(failureCallback, successCallback) {
-  let pdfParser = new pdf2json.default()
-  pdfParser.on('pdfParser_dataReady', async (data) => {
-    const pageCount = data && data.formImage && data.formImage.Pages && data.formImage.Pages.length ? data.formImage.Pages.length : 0;
+  let pdfParser = new pdf2json.default();
+  pdfParser.on("pdfParser_dataReady", async (data) => {
+    const pageCount =
+      data &&
+      data.formImage &&
+      data.formImage.Pages &&
+      data.formImage.Pages.length
+        ? data.formImage.Pages.length
+        : 0;
     if (pageCount > parseInt(process.argv[2])) {
       failureCallback();
     } else {
       await successCallback();
     }
-  })
-  pdfParser.loadPDF('build/cv.pdf')
+  });
+  pdfParser.loadPDF("build/cv.pdf");
 }
 
 async function print(scale, subprocess) {
   // Kerning fix source: https://docs.browserless.io/blog/2020/09/30/puppeteer-print.html
-  const browser = await puppeteer.launch({ executablePath: "/usr/bin/brave", args: ['--font-render-hinting=none'] });
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/brave",
+    args: ["--font-render-hinting=none"],
+  });
   const page = await browser.newPage();
   // TODO: pull URL from stdout of dev command
-  await page.goto('http://localhost:3000');
+  await page.goto("http://localhost:3000");
 
   try {
-    fs.accessSync('build');
+    fs.accessSync("build");
   } catch {
-    fs.mkdirSync('build');
+    fs.mkdirSync("build");
   }
 
   try {
-    fs.unlinkSync('build/cv.pdf');
-  } catch { } finally {
-    await page.pdf({ path: 'build/cv.pdf', format: 'letter', scale: scale });
+    fs.unlinkSync("build/cv.pdf");
+  } catch {
+  } finally {
+    await page.pdf({ path: "build/cv.pdf", format: "letter", scale: scale });
     await browser.close();
     console.log(`pdf saved with scale ${scale}`);
-    await pageCount(() => {
-      console.log(`page count too high with scale ${scale}`);
-      print(scale - 0.01, subprocess);
-    }, () => {
-      subprocess.kill('SIGINT');
-      console.log("dev server stopped");
-    })
+    await pageCount(
+      () => {
+        console.log(`page count too high with scale ${scale}`);
+        print(scale - 0.01, subprocess);
+      },
+      () => {
+        subprocess.kill("SIGINT");
+        console.log("dev server stopped");
+      }
+    );
   }
 }
 
-const subprocess = child_process.spawn("npm", ["run", "dev"])
+const subprocess = child_process.spawn("npm", ["run", "dev"]);
 let printBegun = false;
-subprocess.stdout.on('data', (data) => {
+subprocess.stdout.on("data", (data) => {
   if (!printBegun && String(data).match(/> Local: http:\/\//g)) {
     printBegun = true;
-    console.log("dev server started")
+    console.log("dev server started");
     print(1, subprocess);
   }
-})
+});
